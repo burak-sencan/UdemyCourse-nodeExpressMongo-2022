4de51a9cd3447ea9bccedc26126c16d5ff686677
burakSencan <burak.sencan.1995@hotmail.com>
2022-03-17 22:22:24 +0300

    10 Authentication, Authorization and Security

---
+++ 008-Mongoose/controllers/authController.js
@@ -0,0 +1,18 @@
+const User = require('../models/userModel')
+
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body)
+    res.status(201).json({
+      status:"succes",
+      data: {
+        user: newUser,
+      },
+    })
+  } catch (error) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    })
+  }
+}
